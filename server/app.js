13c0676df7689ea1f5f24cfe1706ba09bb680c55
KIMMUSIC <ldgyuuuuu@gmail.com>
2021-11-09 14:07:16 +0900

    t

--- server/app.js
+++ server/app.js
@@ -4,16 +4,13 @@ var ejs = require('ejs');
 var session = require('express-session'); //session module;
 var MysqlStore = require('express-mysql-session')(session);
 const db = require('./db');
-<<<<<<< HEAD
+
 const iosoc = require('express-socket.io-session');
 const bodyPaser= require('body-parser');
 
 var server = require('http').createServer(app);
 global.server = server;
-=======
 
-const iosoc = require('express-socket.io-session');
->>>>>>> 106051838b8ab19a68f52bbd304add8b00ffe933
 
 var server = require('http').createServer(app);
 global.server = server;
@@ -23,12 +20,9 @@ const search = require('./router/search.js');
 const login = require('./router/login.js'); 
 const board = require('./router/board.js'); 
 const detail = require('./router/detail.js'); 
-<<<<<<< HEAD
+
 const chat = require('./router/chat.js').router
-=======
-const chat = require('./router/chat.js').router;
-// const chatio = require('./router/chat.js').io(server);
->>>>>>> 106051838b8ab19a68f52bbd304add8b00ffe933
+
 
 app.use(session({
     secret: '1234@', //it can be same as cookie setting
@@ -45,16 +39,11 @@ app.use(detail);
 app.use('/static', express.static(__dirname + '/public'))
 app.use('/users', express.static('uploads'));
 app.use(chat);
-<<<<<<< HEAD
+
 app.use(iosoc(session, {autoSave : true}));
 app.use(bodyPaser.urlencoded({limit: '5mb', extended: false, parameterLimit: 10000}));
 
 
-=======
-
-app.use(iosoc(session, {autoSave : true}));
-// app.use(chatio);
->>>>>>> 106051838b8ab19a68f52bbd304add8b00ffe933
 
 
 app.set('view engine', 'ejs');
